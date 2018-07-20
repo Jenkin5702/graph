fae59bd2d0beef64678f0f80a7bc0d046475831c
kisetsu <17694805702@163.com>
2018-07-20 14:43:15 +0800

    initial commit

---
+++ dijstra.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+
+def dj(C):
+    A=np.array(C)
+    m = A.shape[0]
+    r = range(0, m)
+    while True:
+        for i in r:
+            finished = True
+            for j in r:
+                for k in r:
+                    if i != j and A[i, k] + A[k, j] < A[i, j]:
+                        A[i, j] = A[i, k] + A[k, j]
+                        A[j, i] = A[i, k] + A[k, j]
+                        finished = False
+            if finished:
+                return A
+
+a = np.array([[100, 5, 9, 100, 100],
+              [5, 100, 3, 100, 100],
+              [9, 3, 100, 100, 7],
+              [100, 100, 100, 100, 5],
+              [100, 100, 7, 5, 100]])
+
+print dj(a)
